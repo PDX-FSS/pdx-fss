2e5d9b5241208111edd092c0c4e1908f696be3c1
SMRisser <sarah.risser@gmail.com>
2026-09-24 10:53:04 -0700

    Fix Netlify form submission route

--- components/StopSuperSpeedersEndorsementForm.js
+++ components/StopSuperSpeedersEndorsementForm.js
@@ -11,7 +11,7 @@ export default function StopSuperSpeedersEndorsementForm() {
     const formData = new FormData(form);
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/__forms.html", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
